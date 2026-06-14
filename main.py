2c7a67503cfd61179b6e6e8eb32be45491fc6f65
Vansh <vanshchoudhary2744@gmail.com>
2026-06-15 00:11:12 +0530

    basics of agents

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain.agents import create_agent
 
 load_dotenv()
 
 llm = ChatGroq(model="llama-3.1-8b-instant")
 
-res = llm.invoke("hello")
-
-print(res.content)
+agent = create_agent(
+    model=llm,
+    tools=[],
+    system_prompt="You are an therapy assistant"
+)
